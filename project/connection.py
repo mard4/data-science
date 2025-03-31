5bdae922294ee70356a5d6db78bddaff7315f9cb
mardeen <martina.dangelo@live.it>
2025-03-31 23:35:38 +0200

    Stop tracking project/connection.py

--- project/connection.py
+++
@@ -1,7 +0,0 @@
-DB_HOST = "bronto.ewi.utwente.nl"
-DB_NAME = "dab_ds2425-2a_10"  
-DB_USER = "dab_ds2425-2a_10"       
-DB_PASS = "FYeq6/bll9ZFREsS"       
-DB_PORT = "5432"                # Default PostgreSQL port
-
-Patate = 1
